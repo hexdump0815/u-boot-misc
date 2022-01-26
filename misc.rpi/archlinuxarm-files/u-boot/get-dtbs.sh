ef8319e269eca629a31025909f366a983f3b8841
hexdump <hexdump0815@googlemail.com>
2022-01-26 22:46:27 +0100

    rpi: v2022.01 - seems to work
    a new try to get a working u-boot on the rpi4 - it seems to work
    as it boots the kernel which booted with another u-boot before
    now fine as well ... all this is inspired by archlinux arm and
    i added a lot of its files as reference as well plus the dtb and
    bootloader files used for my testing - test was done with an old
    1gb rpi4

---
+++ misc.rpi/archlinuxarm-files/u-boot/get-dtbs.sh
@@ -0,0 +1,5 @@
+_commit=fb0bfa6a669745578041e838fc73cc17e8c543c9
+wget https://github.com/raspberrypi/firmware/raw/${_commit}/boot/bcm2710-rpi-3-b.dtb
+wget https://github.com/raspberrypi/firmware/raw/${_commit}/boot/bcm2710-rpi-3-b-plus.dtb
+wget https://github.com/raspberrypi/firmware/raw/${_commit}/boot/bcm2710-rpi-cm3.dtb
+wget https://github.com/raspberrypi/firmware/raw/${_commit}/boot/bcm2711-rpi-4-b.dtb
